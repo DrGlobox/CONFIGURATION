f12425a8335f88b89a8783869755f8f6e6684480
DrGlobox <locap25@gmail.com>
2012-12-10 23:10:47 +0100

    .vim/

---
+++ diff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "diff .zshrc"
+diff .zshrc ~/.zshrc
+
+echo "diff .bashrc"
+diff .bashrc ~/.bachrc
+
+echo "diff .vimrc"
+diff .vimrc ~/.vimrc
+
+echo "diff .vim/"
+diff .vim/ ~/.vim
+
+echo "diff .gntrc"
+diff .gntrc ~/.gntrc
+
