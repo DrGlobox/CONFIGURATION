ede8e1895450effb124a83b9c41effa60a768eb2
DrGlobox <locap25@gmail.com>
2013-01-31 21:17:17 +0100

    add vundle git plugin

--- plugin-vim-install.sh
+++ plugin-vim-install.sh
@@ -1,2 +1,3 @@
 echo "yaourt -S vim-nerdtree vim-supertab vim-colorsamplerpack"
 yaourt -S vim-nerdtree vim-supertab vim-colorsamplerpack
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
